11aa46ea55b975d3859c006a1a023fb1af2588cc
HoanNam <m8mi0909@gmail.com>
2025-06-11 18:07:54 +0700

    Xây dựng UI phần Header #3

---
+++ src/components/Button/index.js
@@ -0,0 +1,67 @@
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+  to,
+  href,
+  text = false,
+  primary = false,
+  outline = false,
+  rounded = false,
+  disabled = false,
+  small = false,
+  large = false,
+  children,
+  leftIcon,
+  rightIcon,
+  className,
+  onClick,
+  ...passProps
+}) {
+  let Comp = 'button';
+  const props = {
+    onClick,
+    ...passProps,
+  };
+
+  // Remove event listener when button is disabled
+  if (disabled) {
+    Object.keys(props).forEach((key) => {
+      if (key.startsWith('on') && typeof props[key] === 'function') {
+        delete props[key];
+      }
+    });
+  }
+
+  if (to) {
+    Comp.to = to;
+    Comp = Link;
+  } else if (href) {
+    Comp.href = href;
+    Comp = 'a';
+  }
+
+  const classes = cx('wrapper', {
+    text,
+    primary,
+    outline,
+    rounded,
+    disabled,
+    small,
+    large,
+    [className]: className,
+  });
+
+  return (
+    <Comp className={classes} {...props}>
+      {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+      <span className={cx('title')}>{children}</span>
+      {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+    </Comp>
+  );
+}
+
+export default Button;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -2,9 +2,14 @@ import { useState } from 'react';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleXmark,
+  faMagnifyingGlass,
+  faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 
 import images from '~/assets/images';
+import Button from '~/components/Button';
 import styles from './Header.module.scss';
 import AccountItem from '~/components/AccountItem';
 import { PopperWrapper } from '~/components/Popper';
@@ -29,14 +34,27 @@ function Header() {
             <div className={cx('search-result')} tabIndex="-1" {...attrs}>
               <PopperWrapper>
                 <h4 className={cx('search-title')}>Accounts</h4>
-                <AccountItem src={images.searchAvatars[0]} alt="Lakers" name="Lakers" username="lakers" />
-                <AccountItem src={images.searchAvatars[0]} alt="Lakers" name="Lakers" username="lakers" />
+                <AccountItem
+                  src={images.searchAvatars[0]}
+                  alt="Lakers"
+                  name="Lakers"
+                  username="lakers"
+                />
+                <AccountItem
+                  src={images.searchAvatars[0]}
+                  alt="Lakers"
+                  name="Lakers"
+                  username="lakers"
+                />
               </PopperWrapper>
             </div>
           )}
         >
           <div className={cx('search')}>
-            <input placeholder="Search accounts and videos" spellCheck={false} />
+            <input
+              placeholder="Search accounts and videos"
+              spellCheck={false}
+            />
             <button className={cx('clear')}>
               <FontAwesomeIcon icon={faCircleXmark} />
             </button>
@@ -48,7 +66,10 @@ function Header() {
           </div>
         </Tippy>
 
-        <div className={cx('actions')}></div>
+        <div className={cx('actions')}>
+          <Button text>Upload</Button>
+          <Button primary>Log in</Button>
+        </div>
       </div>
     </header>
   );
